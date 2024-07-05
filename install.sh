66cb7c1c4b45af2927f8b454d3434bd8ec60ffcc
koalazf99 <2275008634@qq.com>
2024-07-05 18:00:06 +0800

    update installation

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-cd transformers
-pip install -e .
-cd ..
 pip install -r requirements.txt
-
-# Note: You can only run this part if you only want to do inference.
-cd chameleon
+cd transformers
 pip install -e .
